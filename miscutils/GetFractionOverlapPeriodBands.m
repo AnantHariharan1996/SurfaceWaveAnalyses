18dc1420a19e67f33b6d6fd71fb381d2c6268aac
Anant Hariharan <anant.hariharan.1996@gmail.com>
2023-12-16 01:24:10 -0600

    Update GetFractionOverlapPeriodBands.m

--- miscutils/GetFractionOverlapPeriodBands.m
+++ miscutils/GetFractionOverlapPeriodBands.m
@@ -1,6 +1,27 @@
-function fracoverlap = GetFractionOverlapPeriodBands(tlo1,thi1,tlo2,thi2)
+function [fracoverlap] = GetFractionOverlapPeriodBands(tlo1,thi1,tlo2,thi2)
 
-[ overlapYN] =CheckforOverlappingPeriodBand(tlo1,thi1,tlo2,thi2)
+overlapYN = CheckforOverlappingPeriodBand(tlo1,thi1,tlo2,thi2)
 
-if overlap
+if overlapYN == 1 
+
+DynRange1 = thi1-tlo1; 
+DynRange2 = thi2-tlo2;
+% only care about larger of the two
+Diff1 = thi1-tlo2; 
+Diff2 = thi2-tlo1;
+% one of the above is negative. dont
+% want that 
+
+fracoverlap = max([Diff1 Diff2])/...
+max([DynRange1 DynRange2]);
+
+elseif overlapYN == 0
+
+fracoverlap=0;
+
+else
+
+error('the check for overlapping period bands failed.')
+
+end 
 end
